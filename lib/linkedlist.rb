a3cdeed6caedbd529c0830f2442f545aceccfcb1
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-12-09 21:50:43 +0800

    Create append function to linked list

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -1,1 +1,43 @@
 # frozen_string_literal: true
+
+require_relative 'node'
+
+module List
+  # Represent a linked list
+  class LinkedList
+    include List
+
+    attr_reader :head, :tail
+
+    def initialize
+      @head = nil
+      @tail = nil
+    end
+
+    def append(val)
+      if @head.nil?
+        @head = Node.new
+        @head.value = val
+      elsif @tail.nil?
+        @tail = Node.new
+        @tail.value = val
+        @head.next_node = @tail
+      else
+        new = Node.new
+        new.value = val
+        @tail.next_node = new
+        @tail = new
+      end
+    end
+
+    def to_s
+      node = @head
+
+      until node.nil?
+        print "(#{node.value}) -> "
+        node = node.next_node
+      end
+      puts 'nil'
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -1,1 +1,14 @@
 # frozen_string_literal: true
+
+require_relative 'lib/linkedlist'
+
+list = List::LinkedList.new
+
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
+
+puts list
